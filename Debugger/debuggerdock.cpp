03a8af25304d9d53a3e9e6e476bac18fd20cf9b1
adamko222 <adam.melikant@gmail.com>
2020-12-27 21:22:57 +0100

    update, debugger view, start

--- Debugger/debuggerdock.cpp
+++ Debugger/debuggerdock.cpp
@@ -1,6 +1,32 @@
 #include "debuggerdock.h"
 
-DebuggerDock::DebuggerDock()
+DebuggerDock::DebuggerDock(QWidget *parent) : QWidget(parent)
 {
+    base = new QWidget(this);
+    base->setWindowTitle("debugger");
 
+    base->setLayout(buildForm());
+
+
+}
+
+DebuggerDock::~DebuggerDock(){}
+
+QLayout *DebuggerDock::buildForm(){
+    // buildDebuggerWindow();
+    source_view = new PlainTextEdit();
+    QHBoxLayout *layout = new QHBoxLayout();
+
+    layout->addWidget(source_view);
+
+    return layout;
+}
+
+QWidget *DebuggerDock::buildDebuggerWindow(){
+
+    debugwindow = new QWidget();
+
+    next = new QPushButton();
+
+    return debugwindow;
 }
--- Debugger/debuggerdock.h
+++ Debugger/debuggerdock.h
@@ -2,12 +2,42 @@
 #define DEBUGGERDOCK_H
 
 #include <QDockWidget>
+#include <QWidget>
+#include <QPushButton>
+#include <QLabel>
 
-class DebuggerDock : public QDockWidget
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QFormLayout>
+
+
+#include "Widgets/PlainTextEdit/plaintextedit.h"
+
+class DebuggerDock : public QWidget
 {
     Q_OBJECT
 public:
-    DebuggerDock();
+    DebuggerDock(QWidget *parent = nullptr);
+    ~DebuggerDock();
+
+
+private:
+
+    QWidget *base;
+    PlainTextEdit *source_view;
+    QPushButton *next;
+    QWidget *debugwindow;
+
+    QLayout *buildForm();
+    QWidget *buildDebuggerWindow();
+
+
+
+
+
 };
+
+
+
 
 #endif // DEBUGGERDOCK_H
